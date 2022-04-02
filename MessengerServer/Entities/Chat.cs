48d841544d5eb4ad10ff20acceaec46da41237d1
Danylo Sribnyi <Danil.sribny@gmail.com>
2022-04-02 17:09:30 +0300

    Created Entities of DB

---
+++ MessengerServer/Entities/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MessengerServer.Entities
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        // nav property
+        public IEnumerable<Message> Messages { get; set; }
+        public IEnumerable<ChatsUsers> Users { get; set; }
+    }
+}
---
+++ MessengerServer/Entities/Image.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessengerServer.Entities
+{
+    public class Image
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+
+        // nav property
+        public int? UserId { get; set; }
+        public User UserAvatar { get; set; }
+
+
+        public int? MessageId { get; set; }
+        public Message Message { get; set; }
+    }
+}
